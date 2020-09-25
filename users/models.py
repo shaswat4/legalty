d5fe1bb24eed501be41b86a2972147a6812d3121
Shaswat Agrawal <shaswatagrawl01@gmail.com>
2020-09-26 01:06:22 +0530

    Created account model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class account(models.Model):
+
+    username =  models.CharField( max_length = 20 )
+    password =  models.CharField( max_length = 30 )
+    is_lawyer = models.BooleanField( default= False )
